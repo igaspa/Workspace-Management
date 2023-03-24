6c2dd74110806124236273426c9919a802e4fb91
Ivana <gasparovivana@outlook.com>
2023-03-24 16:32:14 +0100

    #DRT-13 Create notification_template model and migration

---
+++ server/database/migrations/07-create-notification-template.js
@@ -0,0 +1,38 @@
+'use strict';
+const { schemeName } = require('../../utils/constants');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('notification_template', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      template: {
+        type: Sequelize.JSONB
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, {
+      schema: schemeName
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`DROP TABLE IF EXISTS ${schemeName}.notification_template;`);
+  }
+};

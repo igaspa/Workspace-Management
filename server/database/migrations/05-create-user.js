7ba7734178ac57f850f76e4ae09143dffa6ca394
Ivana <gasparovivana@outlook.com>
2023-03-24 14:55:52 +0100

    #DRT-3 create User model and migration

---
+++ server/database/migrations/05-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+const { schemeName } = require('../../utils/constants');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      role_id: {
+        type: Sequelize.UUID
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, {
+      schema: schemeName
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`DROP TABLE ${schemeName}.user;`);
+  }
+};

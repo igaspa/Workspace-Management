fe2d32a46dca5293d3ed8abbf0f79605511ebd65
Lovre Begovic <begovic.lovre@gmail.com>
2023-03-27 13:44:47 +0200

    #DRT-31 - Add errorMiddleware to app.js

--- server/app.js
+++ server/app.js
@@ -3,5 +3,11 @@ const app = express();
 const { errorMiddleware } = require('./middleware/error-handlers');
 
 app.use(express.json());
+app.use(errorMiddleware);
+
+// throw error 404 for pages that do not exist
+app.all('*', (_req, res) => {
+  return res.sendStatus(404);
+});
 
 module.exports = app;

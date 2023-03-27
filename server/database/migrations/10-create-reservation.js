05bed9fe68040987a689b32ca3dd2602b927bb70
Lovre Begovic <begovic.lovre@gmail.com>
2023-03-27 10:41:59 +0200

    #DRT_6 - Create reservation model and migration

---
+++ server/database/migrations/10-create-reservation.js
@@ -0,0 +1,43 @@
+'use strict';
+const { schemeName } = require('../../utils/constants');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('reservation', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      working_space_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      reservation_start: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      reservation_end: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, {
+      schema: schemeName
+    });
+  },
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.sequelize.query(`DROP TABLE IF EXISTS ${schemeName}.reservation;`);
+  }
+};
---
+++ server/database/models/reservation.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class reservation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  reservation.init({
+    id: DataTypes.UUID,
+    userId: DataTypes.UUID,
+    workingSpaceId: DataTypes.INTEGER,
+    reservationStart: DataTypes.DATE,
+    reservationEnd: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'reservation'
+  });
+  return reservation;
+};

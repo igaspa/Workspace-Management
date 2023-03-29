2e2e27a27b315bba7367fd696d8b86ca8cb5b16a
Ivana <gasparovivana@outlook.com>
2023-03-29 10:26:01 +0200

    Add new routers into main router

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,8 @@
 const workingSpaceRouter = require('./working-space');
 const notificationTemplateRouter = require('./notification-template');
 const notificationRouter = require('./notification');
+const areaRouter = require('./area');
+const locationRouter = require('./location');
 
 const express = require('express');
 const router = express.Router();
@@ -8,5 +10,7 @@ const router = express.Router();
 router.use('/working-space', workingSpaceRouter);
 router.use('/notification-template', notificationTemplateRouter);
 router.use('/notification', notificationRouter);
+router.use('/area', areaRouter);
+router.use('/location', locationRouter);
 
 module.exports = router;

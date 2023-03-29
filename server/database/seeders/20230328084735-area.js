a16cc58e4877fdcd42fc9b6294e0d89490e4dab7
Ivana <gasparovivana@outlook.com>
2023-03-29 10:25:48 +0200

    Add timestamps into area and location

--- server/database/seeders/20230328084735-area.js
+++ server/database/seeders/20230328084735-area.js
@@ -9,21 +9,27 @@ module.exports = {
         name: 'East Wing',
         location_id: '5419ce7d-c415-480d-a980-cd169559bc85',
         floor: 'First Floor',
-        image: 'https://drive.google.com/file/d/1Z5CBJ8I0Q6s_nM2Lfax95zj8vQiGgUf6/view?usp=share_link'
+        image: 'https://drive.google.com/file/d/1Z5CBJ8I0Q6s_nM2Lfax95zj8vQiGgUf6/view?usp=share_link',
+        created_at: new Date(),
+        updated_at: new Date()
       },
       {
         id: '43b38b33-143e-4acd-84ac-f8108a279589',
         name: 'West Wing',
         location_id: '5419ce7d-c415-480d-a980-cd169559bc85',
         floor: 'First Floor',
-        image: 'https://drive.google.com/file/d/1wt0xNQGs4Ud5vi37oiJqEZIOgXuyy4kq/view?usp=share_link'
+        image: 'https://drive.google.com/file/d/1wt0xNQGs4Ud5vi37oiJqEZIOgXuyy4kq/view?usp=share_link',
+        created_at: new Date(),
+        updated_at: new Date()
       },
       {
         id: '1b880839-990c-4798-9f2d-dfe7b64b23cf',
         name: 'Third Floor',
         location_id: '5419ce7d-c415-480d-a980-cd169559bc85',
         floor: 'Third Floor',
-        image: 'https://drive.google.com/file/d/1v0m6_cdwT4DqUehUiA7s-rCLuDUJ7yjh/view?usp=share_link'
+        image: 'https://drive.google.com/file/d/1v0m6_cdwT4DqUehUiA7s-rCLuDUJ7yjh/view?usp=share_link',
+        created_at: new Date(),
+        updated_at: new Date()
       }
     ]);
   },

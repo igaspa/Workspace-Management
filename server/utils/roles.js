a9af38c040d9fb6b771415481c27974253fe1792
Ivana <gasparovivana@outlook.com>
2023-04-03 15:55:00 +0200

    #DRT-24 Add roles for endpoints

---
+++ server/utils/roles.js
@@ -0,0 +1,7 @@
+const roles = {
+  administrator: 'd2477f3b-d4ce-4269-9221-0ad479fd45ad',
+  lead: '0bdd577d-c8e5-4973-a6e3-6cf806d8a601',
+  employee: 'c40910c3-c2cf-439f-8d5b-be62d7c548be'
+};
+
+exports.roles = roles;

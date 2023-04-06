55c108f90ed9924326bddb0cb6bf776f1bd43d71
Lovre Begovic <begovic.lovre@gmail.com>
2023-04-06 10:00:10 +0200

    General - Minor change on model update in general controller

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -87,7 +87,12 @@ module.exports.createModel = async (Model, req, res) => {
 };
 
 module.exports.updateModel = async (Model, req, res) => {
-  const updatedModel = await Model.update(req.body, {
+  const model = await Model.findOne({
+    where: { id: req.params.id }
+  });
+  console.log(model);
+
+  const [updatedModel, _updatedData] = await Model.update(req.body, {
     where: { id: req.params.id },
     returning: true
   });

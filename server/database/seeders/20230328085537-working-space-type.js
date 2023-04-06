fa841189558f8e14947e23fd8688004c48cefc63
Lovre Begovic <begovic.lovre@gmail.com>
2023-04-06 11:42:38 +0200

    General - Add additional workspaceType seed

--- server/database/seeders/20230328085537-working-space-type.js
+++ server/database/seeders/20230328085537-working-space-type.js
@@ -3,7 +3,7 @@ const { tableName } = require('../../utils/constants');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up (queryInterface, _Sequelize) {
     return queryInterface.bulkInsert(tableName.workspaceType, [
       {
         id: '1dcdda55-3a43-4dc7-bc0d-0ccf99350502',
@@ -11,11 +11,18 @@ module.exports = {
         name: 'Phone Booth',
         created_at: new Date(),
         updated_at: new Date()
+      },
+      {
+        id: '07c0865e-f27c-414e-bd0b-ed5178d9aeb5',
+        reservation_time: '00:15:00',
+        name: 'Conference Room',
+        created_at: new Date(),
+        updated_at: new Date()
       }
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down (queryInterface, _Sequelize) {
     await queryInterface.bulkDelete(tableName.workspaceType, null, {});
   }
 };

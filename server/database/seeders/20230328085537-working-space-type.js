34970c59060e32aa2a84e36c412f861922c3d459
Lovre Begovic <begovic.lovre@gmail.com>
2023-04-07 15:33:08 +0200

    #DRT-11 - Add attribute for max daily and max overall reservation time

--- server/database/seeders/20230328085537-working-space-type.js
+++ server/database/seeders/20230328085537-working-space-type.js
@@ -7,15 +7,25 @@ module.exports = {
     return queryInterface.bulkInsert(tableName.workspaceType, [
       {
         id: '1dcdda55-3a43-4dc7-bc0d-0ccf99350502',
-        reservation_time: '00:15:00',
+        max_reservation_time_daily: '02:00:00',
+        max_reservation_time_overall: '06:00:00',
         name: 'Phone Booth',
         created_at: new Date(),
         updated_at: new Date()
       },
       {
         id: '07c0865e-f27c-414e-bd0b-ed5178d9aeb5',
-        reservation_time: '00:15:00',
+        max_reservation_time_daily: '02:00:00',
+        max_reservation_time_overall: '06:00:00',
         name: 'Conference Room',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 'eb6e1123-f8c4-43d4-9491-fbaa214cb53a',
+        max_reservation_time_daily: '24:00:00',
+        max_reservation_time_overall: '120:00:00',
+        name: 'Desk',
         created_at: new Date(),
         updated_at: new Date()
       }

e8e0aceca61215ae12c92be349204511d3d9d815
Ivana <gasparovivana@outlook.com>
2023-04-11 09:54:55 +0200

    #DRT-36 Add swagger configuration files

--- server/app.js
+++ server/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const router = require('./routers/index');
 const { errorMiddleware } = require('./middleware/error-handler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./workspace-management-swagger.json');
 
 app.use(express.json());
 app.use('/api/v1/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorMiddleware);
 
 // throw error 404 for pages that do not exist
 app.all('*', (_req, res) => {
   return res.sendStatus(404);
 });
-
 module.exports = app;

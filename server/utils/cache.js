7347717a0cdf2032aedddbb973b3ac1a1e1020ea
Ivana <gasparovivana@outlook.com>
2023-04-12 16:27:48 +0200

    #DRT-26 Fix cache bug

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,14 +1,12 @@
 const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 7200 });
 
-module.exports.setCache = (data, key) => {
-  const cachedData = cache.set(data, key);
-  if (cachedData) { console.log(`Cached data: ${key}`); };
-
-  console.log(`Cached data failed: ${key}`);
+exports.setCache = (key, data) => {
+  const cachedData = cache.set(key, data);
+  if (cachedData) { console.log(`Cached data: ${key}`); } else console.log(`Cached data failed: ${key}`);
 };
 
-module.exports.getCache = (key) => {
+exports.getCache = (key) => {
   const cachedData = cache.get(key);
   return cachedData;
 };

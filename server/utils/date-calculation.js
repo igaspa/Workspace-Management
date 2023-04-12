0d0173f086eb2628b624398f25c40a3968e15a03
Ivana <gasparovivana@outlook.com>
2023-04-12 16:29:24 +0200

    #DRT-22 Add date-calculation for email notification

--- server/utils/date-calculation.js
+++ server/utils/date-calculation.js
@@ -4,3 +4,22 @@ exports.convertToMs = (interval) => {
 };
 
 exports.MINIMUM_RESERVATION_INTERVAL = 300000;
+
+exports.calculateStartDate = (startAt) => {
+  return new Date(startAt).toLocaleString('en-US', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  });
+};
+exports.calculateEndDate = (endAt) => {
+  return new Date(endAt).toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  });
+};

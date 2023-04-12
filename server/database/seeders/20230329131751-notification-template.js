de91d8e91806afa58713a03995e5da4abf8444ee
Ivana <gasparovivana@outlook.com>
2023-04-12 16:26:02 +0200

    #DRT-22 Add templates for created, updated and deleted reservation

--- server/database/seeders/20230329131751-notification-template.js
+++ server/database/seeders/20230329131751-notification-template.js
@@ -22,7 +22,74 @@ module.exports = {
                               <tbody>
                                 <h3>Dear {userName},</h3>
     
-                                <p>{workspaceName} you have reserved for {dateTime} has been confirmed.</p><br>
+                                <p><b>{workspaceName}</b> you have reserved for: <br><br> <b>{dateTime}</b> <br> has been confirmed.</p><br>
+                      
+                                Thank you for your reservation,<br>
+                        
+                                <em>Workspace Management</em>
+                              </tbody>
+                          </table>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+        </body>`
+        },
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '855798c2-c224-43df-bc9e-b7b761976674',
+        name: 'reservation_updated_template',
+        template: {
+          title: 'Workspace Management: You reservation has been updated!',
+          body: `<body style="background-color:white">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" 
+                 width="600" bgcolor="AliceBlue">
+              <tbody>
+                  <tr>
+                      <td align="center">
+                          <br />
+                          <table align="center" border="0" cellpadding="50"
+                                 cellspacing="0" class="col-550" width="550">
+                              <tbody>
+                                <h3>Dear {userName},</h3>
+    
+                                <p><b>{workspaceName}</b> you have reserved has been updated to:
+                                 <br><br> <b>{dateTime}</b> <br> has been confirmed.</p><br>
+                      
+                                Thank you for your reservation,<br>
+                        
+                                <em>Workspace Management</em>
+                              </tbody>
+                          </table>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+        </body>`
+        },
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '174cd318-1a47-41a6-9dff-f7a15bda77fe',
+        name: 'reservation_canceled_template',
+        template: {
+          title: 'Workspace Management: You reservation has been canceled!',
+          body: `<body style="background-color:white">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" 
+                 width="600" bgcolor="AliceBlue">
+              <tbody>
+                  <tr>
+                      <td align="center">
+                          <br />
+                          <table align="center" border="0" cellpadding="50"
+                                 cellspacing="0" class="col-550" width="550">
+                              <tbody>
+                                <h3>Dear {userName},</h3>
+    
+                                <p><b>{workspaceName}</b> you have reserved has been canceled.<br><br> 
                       
                                 Thank you for your reservation,<br>
                         
@@ -41,7 +108,7 @@ module.exports = {
     ], {}, { template: { type: new Sequelize.JSONB() } });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down (queryInterface, _Sequelize) {
     await queryInterface.bulkDelete(tableName.notificationTemplate, null, {});
   }
 };

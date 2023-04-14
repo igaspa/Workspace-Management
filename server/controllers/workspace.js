63d9f9fce931561aef16828fd6ead6faa6f4ae60
Lovre Begovic <begovic.lovre@gmail.com>
2023-04-14 19:16:48 +0200

    DRT-10 - Add support for filtering workspaces by business and availability

--- server/controllers/workspace.js
+++ server/controllers/workspace.js
@@ -1,7 +1,85 @@
 const workspaceService = require('../services/workspace');
 const responseMessage = require('../utils/response-messages');
-const { workspace } = require('../database/models');
+const { workspace, workspaceType, reservation, sequelize } = require('../database/models');
+const { Op } = require('sequelize');
 const generalController = require('./general');
+const { errors } = require('../utils/errors');
+const { EXCLUDE_LIST } = require('../utils/constants');
+
+const workspaceCustomIncludeOptions = (queryParams) => {
+  const options = [];
+  const { workspace_type, status, from, until } = queryParams;
+
+  if (workspace_type) {
+    const workspaceTypeName = workspace_type.replace(/_/g, ' ');
+    const term = {
+      model: workspaceType,
+      where: { name: { [Op.iLike]: workspaceTypeName } },
+      attributes: []
+    };
+    options.push(term);
+  }
+
+  if (status && status === 'busy') {
+    const term = {
+      attributes: { exclude: EXCLUDE_LIST },
+      model: reservation
+    };
+    if (from || until) {
+      // Return all workspaces that all reserved from-until date range
+      const startTime = from ? new Date(from) : new Date();
+      const endTime = until ? new Date(until) : new Date(new Date(startTime).setHours(24, 0, 0, 0));
+      term.where = {
+        [Op.or]: [
+          {
+            [Op.and]: [
+              { start_at: { [Op.lt]: endTime } },
+              { end_at: { [Op.gt]: startTime } }
+            ]
+          },
+          { end_at: null }
+        ]
+      };
+    } else {
+      // Return all workspaces that have any kind of active reservations
+      term.where = {
+        [Op.or]: [
+          { end_at: { [Op.gt]: new Date() } },
+          { end_at: null }
+        ]
+      };
+    }
+    options.push(term);
+  }
+
+  return options;
+};
+
+const workspaceCustomWhereOptions = (queryParams) => {
+  const options = [];
+  const { from, until, status } = queryParams;
+
+  if (status && status === 'available') {
+    const startTime = from ? new Date(from) : new Date();
+    const endTime = until ? new Date(until) : new Date(new Date(startTime).setHours(24, 0, 0, 0));
+    // Return all workspaces whose reservation is not overlapping with the requested date range
+    const term = {
+      [Op.not]: [
+        sequelize.literal(`EXISTS (
+          SELECT *
+          FROM reservation
+          WHERE
+            reservation.workspace_id = workspace.id AND
+            reservation.start_at < ${sequelize.escape(endTime)} AND
+            reservation.end_at > ${sequelize.escape(startTime)}
+        )`)
+      ],
+      permanently_reserved: false
+    };
+    options.push(term);
+  }
+  return options;
+};
 
 exports.createWorkspaces = async (req, res) => {
   await workspaceService.createMultipleWorkspaces(req);
@@ -15,7 +93,15 @@ exports.createOneWorkspace = async (req, res) => {
 };
 
 module.exports.getAllWorkspaces = async (req, res) => {
-  await generalController.findAllModels(workspace, null, req, res);
+  const customIncludeOptions = workspaceCustomIncludeOptions(req.query);
+  const customWhereOptions = workspaceCustomWhereOptions(req.query);
+
+  const customOptions = {
+    include: customIncludeOptions,
+    where: customWhereOptions
+  };
+
+  await generalController.findAllModels(workspace, customOptions, req, res);
 };
 
 module.exports.getWorkspace = async (req, res) => {

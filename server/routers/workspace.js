cf274aea037cf583f2a6d0ea92570c2a312638ce
Ivana <gasparovivana@outlook.com>
2023-04-17 16:30:44 +0200

    #DRT-10 Add restriction for workspaces

--- server/routers/workspace.js
+++ server/routers/workspace.js
@@ -1,36 +1,40 @@
+const { roles } = require('../utils/roles');
 const { callbackErrorHandler } = require('../middleware/error-handler');
 const { bodyValidator, paramValidator, bodyValidatorAdditionalAttribute } = require('../middleware/joi-validator');
 const express = require('express');
+const { authenticateUser, restrictRoles } = require('../middleware/authenticate-user');
 
 const workspaceController = require('../controllers/workspace');
 
 const router = express.Router();
 
+router.use(callbackErrorHandler(authenticateUser));
+
 router
   .route('/area/:id')
-  .delete(paramValidator, callbackErrorHandler(workspaceController.deleteWorkspacesFromArea));
+  .delete(restrictRoles([roles.administrator]), paramValidator, callbackErrorHandler(workspaceController.deleteWorkspacesFromArea));
 
 router
   .route('/location/:id')
-  .delete(paramValidator, callbackErrorHandler(workspaceController.deleteWorkspacesFromLocation));
+  .delete(restrictRoles([roles.administrator]), paramValidator, callbackErrorHandler(workspaceController.deleteWorkspacesFromLocation));
 
 router
   .route('/:id')
-  .get(paramValidator, callbackErrorHandler(workspaceController.getWorkspace))
-  .put(paramValidator, bodyValidator, callbackErrorHandler(workspaceController.updateWorkspace))
-  .delete(paramValidator, callbackErrorHandler(workspaceController.deleteWorkspace));
+  .get(restrictRoles([roles.administrator, roles.employee, roles.lead]), paramValidator, callbackErrorHandler(workspaceController.getWorkspace))
+  .put(restrictRoles([roles.administrator]), paramValidator, bodyValidator, callbackErrorHandler(workspaceController.updateWorkspace))
+  .delete(restrictRoles([roles.administrator]), paramValidator, callbackErrorHandler(workspaceController.deleteWorkspace));
 
 router
   .route('/collection')
-  .post(bodyValidatorAdditionalAttribute, callbackErrorHandler(workspaceController.createWorkspaces));
+  .post(restrictRoles([roles.administrator]), bodyValidatorAdditionalAttribute, callbackErrorHandler(workspaceController.createWorkspaces));
 
 router
   .route('/delete-collection')
-  .post(callbackErrorHandler(workspaceController.deleteMultipleWorkspaces));
+  .post(restrictRoles([roles.administrator]), callbackErrorHandler(workspaceController.deleteMultipleWorkspaces));
 
 router
   .route('/')
-  .post(bodyValidator, callbackErrorHandler(workspaceController.createOneWorkspace))
-  .get(callbackErrorHandler(workspaceController.getAllWorkspaces));
+  .post(restrictRoles([roles.administrator]), bodyValidator, callbackErrorHandler(workspaceController.createOneWorkspace))
+  .get(restrictRoles([roles.administrator, roles.employee, roles.lead]), callbackErrorHandler(workspaceController.getAllWorkspaces));
 
 module.exports = router;

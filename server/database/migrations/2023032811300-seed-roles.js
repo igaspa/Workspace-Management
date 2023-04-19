fc2f025ddf5ec22a8083a3b98619f39f36e3b766
Lovre Begovic <begovic.lovre@gmail.com>
2023-04-19 12:11:48 +0200

    General - Update Roles seed

--- server/database/migrations/2023032811300-seed-roles.js
+++ server/database/migrations/2023032811300-seed-roles.js
@@ -10,16 +10,6 @@ module.exports = {
         id: 'd2477f3b-d4ce-4269-9221-0ad479fd45ad',
         name: 'Administrator',
         allow_multiple_reservations: true
-      },
-      {
-        id: '0bdd577d-c8e5-4973-a6e3-6cf806d8a601',
-        name: 'Lead',
-        allow_multiple_reservations: true
-      },
-      {
-        id: 'c40910c3-c2cf-439f-8d5b-be62d7c548be',
-        name: 'Employee',
-        allow_multiple_reservations: false
       }
     ]);
   },

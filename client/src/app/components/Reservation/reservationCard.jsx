8b2957772917df2174e64eace9d008176d5e1515
Lovre Begovic <begovic.lovre@gmail.com>
2023-05-04 12:18:59 +0200

    #DRT-38 - Add popup Dialog when deleting reservation

--- client/src/app/components/Reservation/reservationCard.jsx
+++ client/src/app/components/Reservation/reservationCard.jsx
@@ -1,25 +1,52 @@
-import { Card, CardContent, Button, Box } from '@mui/material';
+import React from 'react';
+import { Card, CardContent, Button, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 
 export default function ReservationCard ({ reservation, handleDeleteClick, handleUpdateClick }) {
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<Card sx={{ maxWidth: 200 }}>
 			<CardContent sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+
 				<CardContent sx={{ height: 50 }}>
 					<Typography variant="h7" gutterBottom>{reservation.workspace.name}</Typography>
 				</CardContent>
+
 				<CardContent >
 					<Typography sx={{ paddingTop: 2 }}fontSize={12} gutterBottom>{reservation.dateTime}</Typography>
 				</CardContent>
+
 				<CardContent spacing={1}>
 					<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
 						<Button size="small" color="primary" onClick={handleUpdateClick}>
-        Update
+							Update
 						</Button>
-						<Button size="small" color="primary" onClick={handleDeleteClick}>
-        Delete
+						<Button size="small" color="primary" onClick={handleOpen}>
+							Delete
 						</Button>
+
+						<Dialog open={open} onClose={handleClose}>
+							<DialogTitle>Delete Reservation</DialogTitle>
+							<DialogContent>
+								<DialogContentText>
+							Are you sure you want to delete this reservation?
+								</DialogContentText>
+							</DialogContent>
+							<DialogActions>
+								<Button onClick={handleDeleteClick} autoFocus>Delete</Button>
+								<Button onClick={handleClose}>Cancel</Button>
+							</DialogActions>
+						</Dialog>
 					</Box>
 				</CardContent>
 			</CardContent>

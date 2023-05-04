e933b0ca730f85d87900ae30cea683c41de8a837
Lovre Begovic <begovic.lovre@gmail.com>
2023-05-04 12:17:22 +0200

    #DRT-42 - Handle Dashboard errors

--- client/src/app/utils/errors.js
+++ client/src/app/utils/errors.js
@@ -3,5 +3,7 @@ import { errorToast } from '../utils/toastifyNotification';
 
 export function errorHandler (apiError) {
 	if (Array.isArray(apiError.data.details)) apiError.data.details.forEach(err => errorToast(err));
-	else console.log(errorToast(apiError.data.details));
+	else errorToast(apiError.data.details);
+
+	if (apiError.status === 401) return true;
 }

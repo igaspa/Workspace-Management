be974802ac7a59902f4d1269384281c8736a6e4d
Lovre Begovic <begovic.lovre@gmail.com>
2023-05-05 10:22:13 +0200

    General - Delete unnecessary code

--- client/src/app/pages/SignIn/index.jsx
+++ client/src/app/pages/SignIn/index.jsx
@@ -2,15 +2,12 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useCreateLoginMutation } from '../../api/loginApiSlice';
 
 const theme = createTheme();
@@ -52,7 +49,7 @@ export default function SignIn () {
 						alignItems: 'center'
 					}}
 				>
-					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<Avatar sx={{ m: 1, bgcolor: '#5dbea3' }}>
 						<LockOutlinedIcon />
 					</Avatar>
 					<Typography component="h1" variant="h5">
@@ -79,30 +76,14 @@ export default function SignIn () {
 							id="password"
 							autoComplete="current-password"
 						/>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" />}
-							label="Remember me"
-						/>
 						<Button
 							type="submit"
 							fullWidth
 							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
+							sx={{ mt: 3, mb: 2, background: '#5dbea3' }}
 						>
               Sign In
 						</Button>
-						<Grid container>
-							<Grid item xs>
-								<Link href="#" variant="body2">
-                  Forgot password?
-								</Link>
-							</Grid>
-							<Grid item>
-								<Link to="/time-tracker/sign-up">
-									{"Don't have an account? Sign Up"}
-								</Link>
-							</Grid>
-						</Grid>
 					</Box>
 				</Box>
 			</Container>

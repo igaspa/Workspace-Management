d2b2bf94c473c055bba3f914129deba220bb7625
Lovre Begovic <begovic.lovre@gmail.com>
2023-05-16 19:35:35 +0200

    DRT-44 - Enable updating equipment quantity

--- client/src/app/components/Workspace/updateWorkspaceForm.jsx
+++ client/src/app/components/Workspace/updateWorkspaceForm.jsx
@@ -132,6 +132,7 @@ const WorkspaceForm = ({ workspace, onSave, onCancel }) => {
 	return (
 		<>
 			{(areas && workspaceTypes && equipment?.length) &&
+
 		<Box
 			component="form"
 			onSubmit={handleSubmit}

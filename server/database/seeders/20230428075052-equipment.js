88253130442275ba7b7ebb7d8b883db4e76cb3e8
Ivana <gasparovivana@outlook.com>
2023-05-17 10:57:02 +0200

    Add more seeders

--- server/database/seeders/20230428075052-equipment.js
+++ server/database/seeders/20230428075052-equipment.js
@@ -22,6 +22,54 @@ module.exports = {
         name: 'Microphone',
         created_at: new Date(),
         updated_at: new Date()
+      },
+      {
+        id: 'e88af55a-5039-403d-8356-681d7246e487',
+        name: 'iPhone 12',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 'cff24a3f-f656-4d78-9025-55f0e25276bf',
+        name: 'Samsung Galaxy S21',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 'f92b64ff-702d-4323-a47f-b09a55b7c9b9',
+        name: 'Ipad 8',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 'f1293a71-76c5-409c-a75d-1c8fec8f27ea',
+        name: 'Psychological Monitoring Device',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '361ee388-8069-4887-93b7-f85a19e1139e',
+        name: 'D-Link',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '2ea26329-f423-48bb-9e09-6cf861f3c01b',
+        name: 'Logitech G435 Mouse',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '2562ee71-1442-4ce7-9e2b-e311be050199',
+        name: 'Omron Body Scale',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: '6427a70a-4c69-47b9-b252-3dac0bdbddbc',
+        name: 'Power Outlet',
+        created_at: new Date(),
+        updated_at: new Date()
       }
     ]);
   },

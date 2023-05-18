277d4ea26d8bdd521c2b9cf7019ecb1845119fcc
Ivana <gasparovivana@outlook.com>
2023-05-18 11:10:21 +0200

    #DRT-50 Add frontend for area

---
+++ client/src/app/pages/Area/index.jsx
@@ -0,0 +1,9 @@
+import AreaTable from '../../features/Area/AreaTable';
+
+export default function AreaList () {
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly', paddingTop: 20, paddingBottom: 2 }}>
+			<AreaTable />
+		</div>
+	);
+}

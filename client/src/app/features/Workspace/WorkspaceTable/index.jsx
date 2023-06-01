3abc0da19a0a2eafc03b94a5dea8a62625813a3e
PetarTrutanic2 <petar.trutanic@agilathon.com>
2023-06-01 23:06:02 +0200

    Formatted document

--- client/src/app/features/Workspace/WorkspaceTable/index.jsx
+++ client/src/app/features/Workspace/WorkspaceTable/index.jsx
@@ -1,7 +1,22 @@
 import { useState, useRef } from 'react';
-import { CircularProgress, Typography, Box, Dialog, DialogTitle, DialogContent, useMediaQuery, Grid, Container } from '@mui/material';
+import {
+  CircularProgress,
+  Typography,
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  useMediaQuery,
+  Grid,
+  Container
+} from '@mui/material';
 import { successToast } from '../../../utils/toastifyNotification';
-import { useDeleteWorkspaceMutation, useGetWorkspaceSearchListQuery, useGetWorkspacesListQuery, useUpdateWorkspaceMutation } from '../../../api/workspaceApiSlice';
+import {
+  useDeleteWorkspaceMutation,
+  useGetWorkspaceSearchListQuery,
+  useGetWorkspacesListQuery,
+  useUpdateWorkspaceMutation
+} from '../../../api/workspaceApiSlice';
 import { useNavigate } from 'react-router-dom';
 import { errorHandler } from '../../../utils/errors';
 import WorkspaceForm from '../../../components/Workspace/updateWorkspaceForm';
@@ -15,298 +30,286 @@ import SearchButton from '../../../components/Buttons/searchButton';
 import SearchField from '../../../components/Filters/searchField';
 
 const columns = [
-	{
-		id: 'name',
-		label: 'Name',
-		minWidth: 170,
-		align: 'left'
-	},
-	{
-		id: 'type',
-		label: 'Type',
-		minWidth: 150,
-		align: 'left'
-	},
-	{
-		id: 'area',
-		label: 'Area',
-		minWidth: 150,
-		align: 'left'
-	},
-	{
-		id: 'permanentlyReserved',
-		label: 'Permanently Reserved',
-		minWidth: 100,
-		align: 'left'
-	},
-	{
-		id: 'equipment',
-		label: 'Equipment',
-		minWidth: 170,
-		align: 'left'
-	},
-	{
-		id: 'actions',
-		label: 'Actions',
-		minWidth: 170,
-		align: 'center'
-	}
+  {
+    id: 'name',
+    label: 'Name',
+    minWidth: 170,
+    align: 'left'
+  },
+  {
+    id: 'type',
+    label: 'Type',
+    minWidth: 150,
+    align: 'left'
+  },
+  {
+    id: 'area',
+    label: 'Area',
+    minWidth: 150,
+    align: 'left'
+  },
+  {
+    id: 'permanentlyReserved',
+    label: 'Permanently Reserved',
+    minWidth: 100,
+    align: 'left'
+  },
+  {
+    id: 'equipment',
+    label: 'Equipment',
+    minWidth: 170,
+    align: 'left'
+  },
+  {
+    id: 'actions',
+    label: 'Actions',
+    minWidth: 170,
+    align: 'center'
+  }
 ];
 
 const Workspaces = () => {
-	const theme = useTheme();
-	const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-	const navigate = useNavigate();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const navigate = useNavigate();
 
-	const [deleteWorkspace] = useDeleteWorkspaceMutation();
-	const [updateWorkspace] = useUpdateWorkspaceMutation();
+  const [deleteWorkspace] = useDeleteWorkspaceMutation();
+  const [updateWorkspace] = useUpdateWorkspaceMutation();
 
-	const [open, setOpen] = useState(false);
-	const [openForceDelete, setOpenForceDelete] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [openForceDelete, setOpenForceDelete] = useState(false);
 
-	const [selectedId, setSelectedId] = useState();
-	const [selectedWorkspace, setSelectedWorkspace] = useState(null);
+  const [selectedId, setSelectedId] = useState();
+  const [selectedWorkspace, setSelectedWorkspace] = useState(null);
 
-	const [showUpdateForm, setShowUpdateForm] = useState(false);
-	const searchRef = useRef();
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const searchRef = useRef();
 
-	const [page, setPage] = useState(0);
-	const [size, setSize] = useState(10);
-	const [searchData, setSearchData] = useState('');
-	const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const [searchData, setSearchData] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-	const handleCreateWorkspaceButton = () => {
-		navigate('/backoffice/create-workspace');
-	};
+  const handleCreateWorkspaceButton = () => {
+    navigate('/backoffice/create-workspace');
+  };
 
-	const handleDeleteButton = (workspaceId) => {
-		setSelectedId(workspaceId);
-		setOpen(true);
-	};
+  const handleDeleteButton = (workspaceId) => {
+    setSelectedId(workspaceId);
+    setOpen(true);
+  };
 
-	const handleSearch = (event) => {
-		const searchField = new FormData(searchRef.current);
-		setSearchTerm(searchField.get('area'));
-	};
+  const handleSearch = (event) => {
+    const searchField = new FormData(searchRef.current);
+    setSearchTerm(searchField.get('area'));
+  };
 
-	const handleSearchTerm = (event) => {
-		setSearchData(event.target.value);
-	};
+  const handleSearchTerm = (event) => {
+    setSearchData(event.target.value);
+  };
 
-	const handleSearchClear = (event) => {
-		searchRef.current.reset();
-		setSearchTerm('');
-		setSearchData('');
-	};
+  const handleSearchClear = (event) => {
+    searchRef.current.reset();
+    setSearchTerm('');
+    setSearchData('');
+  };
 
-	const handleUpdateButton = (workspace) => {
-		setSelectedWorkspace(workspace);
-		setSelectedId(workspace.id);
-		setShowUpdateForm(true);
-	};
+  const handleUpdateButton = (workspace) => {
+    setSelectedWorkspace(workspace);
+    setSelectedId(workspace.id);
+    setShowUpdateForm(true);
+  };
 
-	const handleChangeRowsPerPage = (event) => {
-		setSize(+event.target.value);
-		setPage(0);
-	};
+  const handleChangeRowsPerPage = (event) => {
+    setSize(+event.target.value);
+    setPage(0);
+  };
 
-	const handleUpdateWorkspace = async (formObject) => {
-		if (!Object.keys(formObject).length) {
-			successToast('No changes made!');
-			setShowUpdateForm(false);
-			setSelectedId('');
-		} else {
-			await updateWorkspace({ id: selectedId, body: formObject })
-				.unwrap()
-				.then((response) => {
-					successToast(response.message);
-					setShowUpdateForm(false);
-					setSelectedId('');
-				})
-				.catch((error) => {
-					const authorizationError = errorHandler(error);
-					if (authorizationError) navigate('/sign-in');
-				});
-		}
-	};
+  const handleUpdateWorkspace = async (formObject) => {
+    if (!Object.keys(formObject).length) {
+      successToast('No changes made!');
+      setShowUpdateForm(false);
+      setSelectedId('');
+    } else {
+      await updateWorkspace({ id: selectedId, body: formObject })
+        .unwrap()
+        .then((response) => {
+          successToast(response.message);
+          setShowUpdateForm(false);
+          setSelectedId('');
+        })
+        .catch((error) => {
+          const authorizationError = errorHandler(error);
+          if (authorizationError) navigate('/sign-in');
+        });
+    }
+  };
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	const handleOpenForceDelete = () => {
-		setOpenForceDelete(true);
-	};
+  const handleOpenForceDelete = () => {
+    setOpenForceDelete(true);
+  };
 
-	const handleCloseForceDelete = () => {
-		setOpenForceDelete(false);
-	};
+  const handleCloseForceDelete = () => {
+    setOpenForceDelete(false);
+  };
 
-	const handlePageChange = (event, value) => {
-		setPage(value);
-	};
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
-	const { data: [workspaces, workspacePages] = [], isError: isWorkspacesError, isLoading: isWorkspacesLoading } = useGetWorkspacesListQuery({
-		...(page && { page: page + 1 }),
-		...(size && { size }),
-		include: ['workspaceType', 'equipment']
-	});
+  const {
+    data: [workspaces, workspacePages] = [],
+    isError: isWorkspacesError,
+    isLoading: isWorkspacesLoading
+  } = useGetWorkspacesListQuery({
+    ...(page && { page: page + 1 }),
+    ...(size && { size }),
+    include: ['workspaceType', 'equipment']
+  });
 
-	const { data: searchWorkspace = [], isError: isWorkspaceSearchError, isLoading: isWorkspaceSearchLoading } = useGetWorkspaceSearchListQuery({
-		name: [searchTerm]
-	});
+  const {
+    data: searchWorkspace = [],
+    isError: isWorkspaceSearchError,
+    isLoading: isWorkspaceSearchLoading
+  } = useGetWorkspaceSearchListQuery({
+    name: [searchTerm]
+  });
 
-	const filteredData = searchTerm.length >= 3 ? searchWorkspace.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase())) : workspaces;
+  const filteredData =
+    searchTerm.length >= 3
+      ? searchWorkspace.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      : workspaces;
 
-	const data = filteredData?.map((el) => {
-		return {
-			id: el.id,
-			name: el.name,
-			type: el.workspaceType.name,
-			area: el.area.name,
-			permanentlyReserved: el.permanentlyReserved.toString(),
-			equipment: el.equipment.length
-				? (
-					el.equipment.map((eq) => (
-						<div key={eq.id}>
-							{eq.name} - Qty: {eq.workspaceEquipment.quantity}
-						</div>
-					))
-				)
-				: (
-					'/'
-				),
-			actions: <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-				<UpdateButton onClick={() => handleUpdateButton(el)} text={'Update'} />
-				<DeleteButton onClick={() => handleDeleteButton(el.id)} text={'Remove'} />
-			</div>
-		};
-	});
+  const data = filteredData?.map((el) => {
+    return {
+      id: el.id,
+      name: el.name,
+      type: el.workspaceType.name,
+      area: el.area.name,
+      permanentlyReserved: el.permanentlyReserved.toString(),
+      equipment: el.equipment.length
+        ? el.equipment.map((eq) => (
+            <div key={eq.id}>
+              {eq.name} - Qty: {eq.workspaceEquipment.quantity}
+            </div>
+          ))
+        : '/',
+      actions: (
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+          <UpdateButton onClick={() => handleUpdateButton(el)} text={'Update'} />
+          <DeleteButton onClick={() => handleDeleteButton(el.id)} text={'Remove'} />
+        </div>
+      )
+    };
+  });
 
-	const count = workspacePages * size - 1;
+  const count = workspacePages * size - 1;
 
-	const handleDeleteClick = async (event) => {
-		event.preventDefault();
-		await deleteWorkspace({ id: selectedId })
-			.unwrap()
-			.then((response) => {
-				handleClose();
-				successToast(response.message);
-			})
-			.catch((error) => {
-				if (error.status === 400) {
-					handleClose();
-					handleOpenForceDelete();
-				} else (errorHandler(error));
-			});
-	};
+  const handleDeleteClick = async (event) => {
+    event.preventDefault();
+    await deleteWorkspace({ id: selectedId })
+      .unwrap()
+      .then((response) => {
+        handleClose();
+        successToast(response.message);
+      })
+      .catch((error) => {
+        if (error.status === 400) {
+          handleClose();
+          handleOpenForceDelete();
+        } else errorHandler(error);
+      });
+  };
 
-	const handleForceDeleteClick = async (event) => {
-		event.preventDefault();
-		await deleteWorkspace({ id: selectedId, forceDelete: true })
-			.unwrap()
-			.then((response) => {
-				handleCloseForceDelete();
-				successToast(response.message);
-			})
-			.catch((error) => {
-				handleCloseForceDelete();
-				errorHandler(error);
-			});
-	};
+  const handleForceDeleteClick = async (event) => {
+    event.preventDefault();
+    await deleteWorkspace({ id: selectedId, forceDelete: true })
+      .unwrap()
+      .then((response) => {
+        handleCloseForceDelete();
+        successToast(response.message);
+      })
+      .catch((error) => {
+        handleCloseForceDelete();
+        errorHandler(error);
+      });
+  };
 
-	return (
-		<Container maxWidth="lg">
-			{isWorkspacesLoading || isWorkspaceSearchLoading
-				? (
-					<CircularProgress />
-				)
-				: isWorkspacesError || isWorkspaceSearchError
-					? (
-						<Typography color="error">Failed to load workspaces.</Typography>
-					)
-					: (
-						<main>
-							<Box
-								spacing={2}
-								direction="row"
-								flexWrap="wrap"
-								margin={0}
-								padding={0}
-							>
-								<Grid component="form" ref={searchRef}
-									container alignItems='center' paddingBottom={1}
-								>
-									<SearchField
-										data={searchWorkspace.map(item => item.name)}
-										name="area"
-										onChange={handleSearchTerm} />
-									<SearchButton onClick={handleSearch}
-										text={'Search'}
-										disabled={(searchData?.length < 3)}/>
-									<DeleteButton onClick={handleSearchClear}
-										text={'Clear'} />
-									<CreateButton onClick={handleCreateWorkspaceButton}
-										text={'Create Workspaces'} />
+  return (
+    <Container maxWidth="lg">
+      {isWorkspacesLoading || isWorkspaceSearchLoading ? (
+        <CircularProgress />
+      ) : isWorkspacesError || isWorkspaceSearchError ? (
+        <Typography color="error">Failed to load workspaces.</Typography>
+      ) : (
+        <main>
+          <Box spacing={2} direction="row" flexWrap="wrap" margin={0} padding={0}>
+            <Grid component="form" ref={searchRef} container alignItems="center" paddingBottom={1}>
+              <SearchField data={searchWorkspace.map((item) => item.name)} name="area" onChange={handleSearchTerm} />
+              <SearchButton onClick={handleSearch} text={'Search'} disabled={searchData?.length < 3} />
+              <DeleteButton onClick={handleSearchClear} text={'Clear'} />
+              <CreateButton onClick={handleCreateWorkspaceButton} text={'Create Workspaces'} />
+            </Grid>
+            <Box spacing={1} direction="row" flexWrap="wrap" margin={0}>
+              {workspaces.length ? (
+                <>
+                  <DefaultTable
+                    columns={columns}
+                    rows={data}
+                    page={page}
+                    count={count}
+                    rowsPerPage={size}
+                    handleChangePage={handlePageChange}
+                    handleChangeRowsPerPage={handleChangeRowsPerPage}
+                    rowsPerPageOptions={[10, 25, 50, 100]}
+                  />
 
-								</Grid>
-								<Box spacing={1} direction="row" flexWrap="wrap" margin={0}>
-									{workspaces.length
-										? (
-											<>
-												<DefaultTable
-													columns={columns}
-													rows={data}
-													page={page}
-													count={count}
-													rowsPerPage={size}
-													handleChangePage={handlePageChange}
-													handleChangeRowsPerPage={handleChangeRowsPerPage}
-													rowsPerPageOptions={[10, 25, 50, 100]}/>
+                  <Prompt
+                    open={open}
+                    onClose={handleClose}
+                    title={'Delete Workspace'}
+                    body={'Are you sure you want to delete this workspace?'}
+                    handleCancel={handleClose}
+                    handleConfirm={handleDeleteClick}
+                  />
 
-												<Prompt
-													open={open}
-													onClose={handleClose}
-													title={'Delete Workspace'}
-													body={'Are you sure you want to delete this workspace?'}
-													handleCancel={handleClose}
-													handleConfirm={handleDeleteClick}
-												/>
+                  <Prompt
+                    open={openForceDelete}
+                    onClose={handleClose}
+                    title={'Force Delete Workspace'}
+                    body={
+                      'There are active reservations for this workspace, if you wish to proceed click "FORCE DELETE"'
+                    }
+                    handleConfirm={handleForceDeleteClick}
+                    handleCancel={handleCloseForceDelete}
+                  />
 
-												<Prompt
-													open={openForceDelete}
-													onClose={handleClose}
-													title={'Force Delete Workspace'}
-													body={'There are active reservations for this workspace, if you wish to proceed click "FORCE DELETE"'}
-													handleConfirm={handleForceDeleteClick}
-													handleCancel={handleCloseForceDelete}
-												/>
-
-												{showUpdateForm && (
-													<Dialog open={showUpdateForm} fullScreen={fullScreen} onClose={() => setShowUpdateForm(false)}>
-														<DialogTitle>Update Workspace</DialogTitle>
-														<DialogContent>
-															<WorkspaceForm
-																workspace={selectedWorkspace}
-																onSave={handleUpdateWorkspace}
-																onCancel={() => setShowUpdateForm(false)}
-															/>
-														</DialogContent>
-													</Dialog>
-												)}
-
-											</>
-										)
-										: (
-											'There are no workspaces...'
-										)}
-								</Box>
-							</Box>
-						</main>)
-			}
-
-		</Container>
-	);
+                  {showUpdateForm && (
+                    <Dialog open={showUpdateForm} fullScreen={fullScreen} onClose={() => setShowUpdateForm(false)}>
+                      <DialogTitle>Update Workspace</DialogTitle>
+                      <DialogContent>
+                        <WorkspaceForm
+                          workspace={selectedWorkspace}
+                          onSave={handleUpdateWorkspace}
+                          onCancel={() => setShowUpdateForm(false)}
+                        />
+                      </DialogContent>
+                    </Dialog>
+                  )}
+                </>
+              ) : (
+                'There are no workspaces...'
+              )}
+            </Box>
+          </Box>
+        </main>
+      )}
+    </Container>
+  );
 };
 
 export default Workspaces;

2ec721b637750e817f115b6b90f9a393da61094b
PetarTrutanic2 <petar.trutanic@agilathon.com>
2023-06-01 17:41:10 +0200

    Extracted admin user seed data

--- server/database/seeders/20230531123608-admin-user.js
+++ server/database/seeders/20230531123608-admin-user.js
@@ -1,46 +1,25 @@
 'use strict';
 const { user, role, userRole } = require('../models');
 const { roles } = require('../../utils/roles');
-
-const ADMIN_PASSWORD = 'test1234';
-const ADMIN_EMAIL = 'admin@example.com';
+const { ADMIN } = require('../seed-data/users');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface) {
+  async up(queryInterface) {
     const { sequelize } = queryInterface;
-    return sequelize.transaction(async transaction => {
-      const adminRole = await role.findOne(
-        { where: { name: roles.administrator } },
-        { transaction }
-      );
+    return sequelize.transaction(async (transaction) => {
+      const adminRole = await role.findOne({ where: { name: roles.administrator } }, { transaction });
+      const adminUser = await user.create(ADMIN, { transaction });
 
-      const adminUser = await user.create({
-        firstName: 'Admin',
-        lastName: 'User',
-        email: ADMIN_EMAIL,
-        password: ADMIN_PASSWORD,
-        phoneNumber: '+15055555555'
-      }, { transaction });
-
-      return userRole.create(
-        { roleId: adminRole.id, userId: adminUser.id },
-        { transaction }
-      );
+      return userRole.create({ roleId: adminRole.id, userId: adminUser.id }, { transaction });
     });
   },
-  async down (queryInterface) {
+  async down(queryInterface) {
     const { sequelize } = queryInterface;
-    return sequelize.transaction(async transaction => {
-      const admin = await user.findOne(
-        { where: { email: ADMIN_EMAIL } },
-        { transaction }
-      );
+    return sequelize.transaction(async (transaction) => {
+      const admin = await user.findOne({ where: { email: ADMIN.email } }, { transaction });
 
-      await userRole.destroy(
-        { where: { userId: admin.id } },
-        { transaction }
-      );
+      await userRole.destroy({ where: { userId: admin.id } }, { transaction });
       return admin.destroy({ transaction });
     });
   }

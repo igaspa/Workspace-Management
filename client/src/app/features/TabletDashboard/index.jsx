21af0e14f1e49891248c0b87cb26e2b96c4fd519
PetarTrutanic2 <petar.trutanic@agilathon.com>
2023-06-01 20:25:42 +0200

    Added tablet dashboard

---
+++ client/src/app/features/TabletDashboard/index.jsx
@@ -0,0 +1,7 @@
+import Container from '@mui/material/Container';
+
+const TabletDashboard = () => {
+  return <Container maxWidth="lg">asd</Container>;
+};
+
+export default TabletDashboard;
--- client/src/app/pages/dashboard/index.jsx
+++ client/src/app/pages/dashboard/index.jsx
@@ -1,5 +1,10 @@
 import Dashboard from '../../features/Dashboard';
+import TabletDashboard from '../../features/TabletDashboard';
+import { ROLES } from '../../utils/constants';
 
 export default function MainPage() {
-  return <Dashboard />;
+  const role = localStorage.getItem('role');
+  const isTablet = role?.length && role.includes(ROLES.TABLET);
+
+  return isTablet ? <TabletDashboard /> : <Dashboard />;
 }

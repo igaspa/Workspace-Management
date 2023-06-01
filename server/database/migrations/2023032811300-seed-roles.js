185ca6e054570dbe866b5f47430ab2b116d6b27e
PetarTrutanic2 <petar.trutanic@agilathon.com>
2023-06-01 18:41:14 +0200

    Moved user seed

--- server/database/migrations/2023032811300-seed-roles.js
+++ server/database/migrations/2023032811300-seed-roles.js
@@ -1,19 +1,25 @@
 /* eslint-disable quotes */
 'use strict';
 const { tableName } = require('../../utils/constants');
+const { role } = require('../models');
+const { roles } = require('../../utils/roles');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, _Sequelize) {
-    return queryInterface.bulkInsert(tableName.role, [
-      {
-        id: 'd2477f3b-d4ce-4269-9221-0ad479fd45ad',
-        name: 'Administrator'
-      }
-    ]);
+  async up(queryInterface) {
+    const { sequelize } = queryInterface;
+    return sequelize.transaction(async (transaction) => {
+      const roleNames = Object.values(roles);
+
+      return Promise.all(
+        roleNames.map(function (roleName) {
+          return role.create({ name: roleName }, { transaction });
+        })
+      );
+    });
   },
 
-  async down (queryInterface, _Sequelize) {
+  async down(queryInterface, _Sequelize) {
     await queryInterface.bulkDelete(tableName.role, null, {});
   }
 };

061a301df0a0cdfc891e0547fcdd24b64a98b7f4
Lovre Begovic <begovic.lovre@gmail.com>
2023-06-07 09:27:52 +0200

    Refactor Location with prettier

--- client/src/app/features/Location/CreateLocation/index.jsx
+++ client/src/app/features/Location/CreateLocation/index.jsx
@@ -9,81 +9,59 @@ import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { successToast } from '../../../utils/toastifyNotification';
 
-export default function CreateLocation () {
-	const [createLocation] = useCreateLocationMutation();
-	const divRef = useRef();
-	const navigate = useNavigate();
+export default function CreateLocation() {
+  const [createLocation] = useCreateLocationMutation();
+  const divRef = useRef();
+  const navigate = useNavigate();
 
-	const handleCancel = (event) => {
-		navigate('/backoffice/location');
-	};
-	const handleConfirm = async (event) => {
-		event.preventDefault();
-		const data = new FormData(divRef.current);
-		await createLocation(
-			{
-				id: uuidv4(),
-				address: data.get('address'),
-				city: data.get('city'),
-				country: data.get('country')
-			})
-			.unwrap()
-			.then((response) => {
-				successToast(response.message);
-				divRef.current.reset();
-			})
-			.catch((error) => {
-				errorHandler(error);
-			});
-	};
+  const handleCancel = (event) => {
+    navigate('/backoffice/location');
+  };
+  const handleConfirm = async (event) => {
+    event.preventDefault();
+    const data = new FormData(divRef.current);
+    await createLocation({
+      id: uuidv4(),
+      address: data.get('address'),
+      city: data.get('city'),
+      country: data.get('country')
+    })
+      .unwrap()
+      .then((response) => {
+        successToast(response.message);
+        divRef.current.reset();
+      })
+      .catch((error) => {
+        errorHandler(error);
+      });
+  };
 
-	return (
-		<>
-			<Container component="main" maxWidth="xs">
-				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center'
-					}}
-				>
-					<Typography component="h1" variant="h5">
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Typography component="h1" variant="h5">
             Create new Location
-					</Typography>
-					<Box component="form" ref={divRef} noValidate sx={{ mt: 1 }}>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="location"
-							label="Location Address"
-							name="address"
-						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="location"
-							label="City"
-							name="city"
-						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="location"
-							label="Country"
-							name="country"
-						/>
-						<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-							<ConfirmButton text={'Confirm'} onClick={handleConfirm}/>
-							<CancelButton text={'Cancel'} onClick={handleCancel}/>
-						</div>
-					</Box>
-				</Box>
-			</Container>
-		</>
-	);
+          </Typography>
+          <Box component="form" ref={divRef} noValidate sx={{ mt: 1 }}>
+            <TextField margin="normal" required fullWidth id="location" label="Location Address" name="address" />
+            <TextField margin="normal" required fullWidth id="location" label="City" name="city" />
+            <TextField margin="normal" required fullWidth id="location" label="Country" name="country" />
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+              <ConfirmButton text={'Confirm'} onClick={handleConfirm} />
+              <CancelButton text={'Cancel'} onClick={handleCancel} />
+            </div>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
--- client/src/app/features/Location/LocationTable/index.jsx
+++ client/src/app/features/Location/LocationTable/index.jsx
@@ -1,5 +1,10 @@
 import { CircularProgress, Typography, Grid, Box, TextField, Container } from '@mui/material';
-import { useGetLocationListQuery, useDeleteLocationMutation, useUpdateLocationMutation, useGetLocationSearchListQuery } from '../../../api/locationApiSlice';
+import {
+  useGetLocationListQuery,
+  useDeleteLocationMutation,
+  useUpdateLocationMutation,
+  useGetLocationSearchListQuery
+} from '../../../api/locationApiSlice';
 import { useNavigate } from 'react-router-dom';
 import UpdateButton from '../../../components/Buttons/updateButton';
 import { successToast } from '../../../utils/toastifyNotification';
@@ -12,235 +17,242 @@ import SearchButton from '../../../components/Buttons/searchButton';
 import SearchField from '../../../components/Filters/searchField';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import styles from '../../../CSS/Button.Module.css'
-
+import styles from '../../../CSS/Button.Module.css';
 
 const columns = [
-	{
-		id: 'address',
-		label: 'Address',
-		minWidth: 170,
-		align: 'left'
-	},
-	{
-		id: 'city',
-		label: 'City',
-		minWidth: 170,
-		align: 'left'
-	},
-	{
-		id: 'country',
-		label: 'Country',
-		minWidth: 170,
-		align: 'left'
-	},
-	{
-		id: 'actions',
-		label: 'Actions',
-		minWidth: 170,
-		align: 'center'
-	}
+  {
+    id: 'address',
+    label: 'Address',
+    minWidth: 170,
+    align: 'left'
+  },
+  {
+    id: 'city',
+    label: 'City',
+    minWidth: 170,
+    align: 'left'
+  },
+  {
+    id: 'country',
+    label: 'Country',
+    minWidth: 170,
+    align: 'left'
+  },
+  {
+    id: 'actions',
+    label: 'Actions',
+    minWidth: 170,
+    align: 'center'
+  }
 ];
 
-export default function LocationTable () {
-	const role = localStorage.getItem('role');
-	const navigate = useNavigate();
-	const [deleteLocation] = useDeleteLocationMutation();
-	const [updateLocation] = useUpdateLocationMutation();
-	const [openDelete, setOpenDelete] = useState(false);
-	const [openUpdate, setOpenUpdate] = useState(false);
-	const [selectedId, setSelectedId] = useState(' ');
-	const [formData, setFormData] = useState('');
-	const [page, setPage] = useState(0);
-	const [size, setSize] = useState(10);
-	const [searchData, setSearchData] = useState('');
-	const [searchTerm, setSearchTerm] = useState('');
-	const divRef = useRef();
-	const searchRef = useRef();
+export default function LocationTable() {
+  const role = localStorage.getItem('role');
+  const navigate = useNavigate();
+  const [deleteLocation] = useDeleteLocationMutation();
+  const [updateLocation] = useUpdateLocationMutation();
+  const [openDelete, setOpenDelete] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [selectedId, setSelectedId] = useState(' ');
+  const [formData, setFormData] = useState('');
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const [searchData, setSearchData] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const divRef = useRef();
+  const searchRef = useRef();
 
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage);
-	};
-	const handleChangeRowsPerPage = (event) => {
-		setSize(+event.target.value);
-		setPage(0);
-	};
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setSize(+event.target.value);
+    setPage(0);
+  };
 
-	const handleCreateClick = () => {
-		navigate('/backoffice/create-location');
-	};
+  const handleCreateClick = () => {
+    navigate('/backoffice/create-location');
+  };
 
-	const handleSearch = (event) => {
-		const searchField = new FormData(searchRef.current);
-		setSearchTerm(searchField.get('location'));
-	};
+  const handleSearch = (event) => {
+    const searchField = new FormData(searchRef.current);
+    setSearchTerm(searchField.get('location'));
+  };
 
-	const handleSearchTerm = (event) => {
-		setSearchData(event.target.value);
-		setPage(0);
-	};
+  const handleSearchTerm = (event) => {
+    setSearchData(event.target.value);
+    setPage(0);
+  };
 
-	const handleSearchClear = (event) => {
-		searchRef.current.reset();
-		setSearchTerm('');
-		setSearchData('');
-	};
+  const handleSearchClear = (event) => {
+    searchRef.current.reset();
+    setSearchTerm('');
+    setSearchData('');
+  };
 
-	const handleClickOpenDelete = (locationId) => {
-		setSelectedId(locationId);
-		setOpenDelete(true);
-	};
+  const handleClickOpenDelete = (locationId) => {
+    setSelectedId(locationId);
+    setOpenDelete(true);
+  };
 
-	const handleClickOpenUpdate = (locationId) => {
-		const chosenLocation = data.find(el => el.id === locationId);
-		setFormData(chosenLocation);
-		setSelectedId(locationId);
-		setOpenUpdate(true);
-	};
+  const handleClickOpenUpdate = (locationId) => {
+    const chosenLocation = data.find((el) => el.id === locationId);
+    setFormData(chosenLocation);
+    setSelectedId(locationId);
+    setOpenUpdate(true);
+  };
 
-	const handleClose = () => {
-		setOpenUpdate(false);
-		setOpenDelete(false);
-	};
-	const { data: [location, pages] = [], isError: isLocationsError, isLoading: isLocationsLoading } = useGetLocationListQuery({
-		...(page && { page: page + 1 }),
-		...(size && { size })
-	});
-	const { data: [searchLocation, searchLocationPages] = [], isError: isLocationSearchError, isLoading: isLocationSearchLoading } = useGetLocationSearchListQuery({
-		name: [searchTerm],
-		...(page && { page: page + 1 }),
-		...(size && { size })
-	});
+  const handleClose = () => {
+    setOpenUpdate(false);
+    setOpenDelete(false);
+  };
+  const {
+    data: [location, pages] = [],
+    isError: isLocationsError,
+    isLoading: isLocationsLoading
+  } = useGetLocationListQuery({
+    ...(page && { page: page + 1 }),
+    ...(size && { size })
+  });
+  const {
+    data: [searchLocation, searchLocationPages] = [],
+    isError: isLocationSearchError,
+    isLoading: isLocationSearchLoading
+  } = useGetLocationSearchListQuery({
+    name: [searchTerm],
+    ...(page && { page: page + 1 }),
+    ...(size && { size })
+  });
 
-	const handleDeleteClick = async (event) => {
-		event.preventDefault();
-		await deleteLocation(selectedId)
-			.unwrap()
-			.then((response) => {
-				handleClose();
-				successToast(response.message);
-			})
-			.catch((error) => {
-				handleClose();
-				errorHandler(error);
-			});
-	};
+  const handleDeleteClick = async (event) => {
+    event.preventDefault();
+    await deleteLocation(selectedId)
+      .unwrap()
+      .then((response) => {
+        handleClose();
+        successToast(response.message);
+      })
+      .catch((error) => {
+        handleClose();
+        errorHandler(error);
+      });
+  };
 
-	const handleUpdateClick = async (event) => {
-		event.preventDefault();
-		const data = new FormData(divRef.current);
-		const objectToUpdate = {
-			...(data.get('address') && { address: data.get('address') }),
-			...(data.get('city') && { city: data.get('city') }),
-			...(data.get('country') && { country: data.get('country') })
-		};
-		await updateLocation({
-			id: selectedId,
-			body: objectToUpdate
-		})
-			.unwrap()
-			.then((response) => {
-				handleClose();
-				successToast(response.message);
-			})
-			.catch((error) => {
-				handleClose();
-				errorHandler(error);
-			});
-	};
-	const count =searchLocationPages ? searchLocationPages * size : pages * size;
-	const filteredData = searchTerm.length >= 3 ? searchLocation.filter(item => item.address.toLowerCase().includes(searchTerm.toLowerCase())) : location;
+  const handleUpdateClick = async (event) => {
+    event.preventDefault();
+    const data = new FormData(divRef.current);
+    const objectToUpdate = {
+      ...(data.get('address') && { address: data.get('address') }),
+      ...(data.get('city') && { city: data.get('city') }),
+      ...(data.get('country') && { country: data.get('country') })
+    };
+    await updateLocation({
+      id: selectedId,
+      body: objectToUpdate
+    })
+      .unwrap()
+      .then((response) => {
+        handleClose();
+        successToast(response.message);
+      })
+      .catch((error) => {
+        handleClose();
+        errorHandler(error);
+      });
+  };
+  const count = searchLocationPages ? searchLocationPages * size : pages * size;
+  const filteredData =
+    searchTerm.length >= 3
+      ? searchLocation.filter((item) => item.address.toLowerCase().includes(searchTerm.toLowerCase()))
+      : location;
 
-	const data = filteredData?.map((el) => ({
-		id: el.id,
-		address: el.address,
-		city: el.city,
-		country: el.country,
-		actions: 
-		<div style={{ display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'center' }}>
-			<EditIcon className={styles.editIcon} onClick={() => handleClickOpenUpdate(el.id)}></EditIcon>
-			<DeleteIcon className={styles.deleteIcon} onClick={() => handleClickOpenDelete(el.id)} ></DeleteIcon>
-		</div>
-	}));
+  const data = filteredData?.map((el) => ({
+    id: el.id,
+    address: el.address,
+    city: el.city,
+    country: el.country,
+    actions: (
+      <div style={{ display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'center' }}>
+        <EditIcon className={styles.editIcon} onClick={() => handleClickOpenUpdate(el.id)}></EditIcon>
+        <DeleteIcon className={styles.deleteIcon} onClick={() => handleClickOpenDelete(el.id)}></DeleteIcon>
+      </div>
+    )
+  }));
 
-	return (
-		<Container maxWidth="lg">
-			{ role.includes('Administrator')
-				? isLocationsLoading || isLocationSearchLoading
-					? (
-						<CircularProgress />
-					)
-					: isLocationsError || isLocationSearchError
-						? (
-							<Typography color="error">Failed to load location.</Typography>
-						)
-						: (<>
-							<Grid component="form" ref={searchRef}
-								container alignItems='center' paddingBottom={1}
-							>
-								<SearchField
-									data={searchLocation.map(item => item.name)}
-									name="location"
-									onChange={handleSearchTerm} />
-								<SearchButton onClick={handleSearch}
-									text={'Search'}
-									disabled={(searchData?.length < 3)}/>
-								<UpdateButton onClick={handleSearchClear}
-									text={'Clear'} />
-								<CreateButton onClick={handleCreateClick}
-									text={'Create Location'}/>
+  return (
+    <Container maxWidth="lg">
+      {role.includes('Administrator') ? (
+        isLocationsLoading || isLocationSearchLoading ? (
+          <CircularProgress />
+        ) : isLocationsError || isLocationSearchError ? (
+          <Typography color="error">Failed to load location.</Typography>
+        ) : (
+          <>
+            <Grid component="form" ref={searchRef} container alignItems="center" paddingBottom={1}>
+              <SearchField data={searchLocation.map((item) => item.name)} name="location" onChange={handleSearchTerm} />
+              <SearchButton onClick={handleSearch} text={'Search'} disabled={searchData?.length < 3} />
+              <UpdateButton onClick={handleSearchClear} text={'Clear'} />
+              <CreateButton onClick={handleCreateClick} text={'Create Location'} />
+            </Grid>
+            <DefaultTable
+              columns={columns}
+              rows={data}
+              page={page}
+              count={count}
+              rowsPerPage={size}
+              handleChangePage={handleChangePage}
+              handleChangeRowsPerPage={handleChangeRowsPerPage}
+              rowsPerPageOptions={[10, 25, 50, 100]}
+            />
+            <Prompt
+              open={openDelete}
+              onClose={handleClose}
+              title={'Remove Location'}
+              body={'Are you sure you want to remove this location?'}
+              handleCancel={handleClose}
+              handleConfirm={handleDeleteClick}
+            />
 
-							</Grid>
-							<DefaultTable
-								columns={columns}
-								rows={data}
-								page={page}
-								count={count}
-								rowsPerPage={size}
-								handleChangePage={handleChangePage}
-								handleChangeRowsPerPage={handleChangeRowsPerPage}
-								rowsPerPageOptions={[10, 25, 50, 100]}/>
-							<Prompt open={openDelete}
-								onClose={handleClose}
-								title={'Remove Location'}
-								body={'Are you sure you want to remove this location?'}
-								handleCancel={handleClose}
-								handleConfirm={handleDeleteClick} />
-
-							<Prompt open={openUpdate}
-								onClose={handleClose}
-								title={'Update Location'}
-								body={<Box
-									component="form" ref={divRef} noValidate sx={{ mt: 1 }}>
-									<TextField
-										margin="normal"
-										fullWidth
-										defaultValue={formData.address || ' '}
-										id="location"
-										label="Location Address"
-										name="address"
-									/>
-									<TextField
-										margin="normal"
-										fullWidth
-										defaultValue={formData.city || ' '}
-										id="location"
-										label="City"
-										name="city"
-									/>
-									<TextField
-										margin="normal"
-										fullWidth
-										defaultValue={formData.country || ' '}
-										id="location"
-										label="Country"
-										name="country"
-									/></Box>}
-								handleCancel={handleClose}
-								handleConfirm={handleUpdateClick} />
-						</>
-						)
-				: navigate('/')}
-		</Container>
-	);
+            <Prompt
+              open={openUpdate}
+              onClose={handleClose}
+              title={'Update Location'}
+              body={
+                <Box component="form" ref={divRef} noValidate sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    defaultValue={formData.address || ' '}
+                    id="location"
+                    label="Location Address"
+                    name="address"
+                  />
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    defaultValue={formData.city || ' '}
+                    id="location"
+                    label="City"
+                    name="city"
+                  />
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    defaultValue={formData.country || ' '}
+                    id="location"
+                    label="Country"
+                    name="country"
+                  />
+                </Box>
+              }
+              handleCancel={handleClose}
+              handleConfirm={handleUpdateClick}
+            />
+          </>
+        )
+      ) : (
+        navigate('/')
+      )}
+    </Container>
+  );
 }

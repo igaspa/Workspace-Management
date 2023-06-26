c2f81c80ffc884e567f32b108916c5185ddf1766
Ivana <gasparovivana@outlook.com>
2023-06-26 09:58:05 +0200

    Remove cron schedule

--- server/server.js
+++ server/server.js
@@ -1,14 +1,9 @@
 const app = require('./app');
-const cron = require("node-cron");
 const { setNotificationTemplate } = require('./services/notification');
 
 const PORT = process.env.PORT || 4000;
 
-cron.schedule("0 * * * *",function (){
-  setNotificationTemplate();
-})
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server listening on the port  ${PORT}`);
-  setNotificationTemplate();
+  await setNotificationTemplate();
 });

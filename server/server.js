1767e0e45f0b5b58bcfd81633a78df8225d6b221
Ivana <gasparovivana@outlook.com>
2023-06-26 09:18:17 +0200

    DRT-28 - Add Cron job for notification templates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const cron = require("node-cron");
+const { setNotificationTemplate } = require('./services/notification');
+
 const PORT = process.env.PORT || 4000;
+
+cron.schedule("0 * * * *",function (){
+  setNotificationTemplate();
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port  ${PORT}`);
+  setNotificationTemplate();
 });

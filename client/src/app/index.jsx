7397d9dd9462c3fa6b9174282002f8f6543cfa5f
Ivana <gasparovivana@outlook.com>
2023-06-26 09:16:33 +0200

    Bugfix - Remove duplicate toast

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -15,18 +15,6 @@ function App () {
 				<Router>
 					<Routes />
 				</Router>
-				<ToastContainer
-					position="bottom-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="dark"
-				/>
 				<ToastContainer />
 			</QueryClientProvider>
 		</ThemeProvider>
